f3184b4bde0a539e274ab60afd1f2243a854d3db
Ang Yak Hng <angyakhng8000@gmail.com>
2023-01-03 18:21:31 +0800

    initial commit

---
+++ functions/src/index.ts
@@ -0,0 +1,12 @@
+import * as functions from "firebase-functions";
+// import {Bot} from 'grammy';
+
+// // Start writing functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+
